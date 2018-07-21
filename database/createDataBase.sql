43a9444e072a8127b13d44845e4c0f70374bcf65
hadidy100 <hadidy100@gmail.com>
2018-07-21 00:52:15 -0700

    add index4, faq2, recipes. index4 has hyperlinks to the other files, but once the team develop the content of the files them we can change the liks in index4 to the updated files.

--- database/createDataBase.sql
+++ database/createDataBase.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE YUMMYTUMMY;
 SHOW DATABASES;
 USE YUMMYTUMMY; # CONNECTING TO THE NEWLY CREATED DATABASE
-
 SHOW TABLES; # TO SHOW ALL THE TABLES IN THE DATABASE
+
 CREATE TABLE images
 (
   imageId int not null AUTO_INCREMENT PRIMARY KEY,
@@ -11,14 +11,23 @@ CREATE TABLE images
 );
 
 create table imageDescription
-  (
+(
     descId int not null AUTO_INCREMENT PRIMARY KEY,
     imageId int not null,
     description varchar (120),
-	  CONSTRAINT 	FOREIGN KEY (imageId) REFERENCES images (imageId)
-		ON DELETE CASCADE
-		ON UPDATE RESTRICT
-  );
+	CONSTRAINT 	FOREIGN KEY (imageId) REFERENCES images (imageId)
+	ON DELETE CASCADE
+	ON UPDATE RESTRICT
+);
 
+create table mealIngredient
+(
+    ingredientId int not null AUTO_INCREMENT PRIMARY KEY,
+    imageId int not null,
+    description VARCHAR (255),
+	CONSTRAINT 	FOREIGN KEY (imageId) REFERENCES images (imageId)
+	ON DELETE CASCADE
+	ON UPDATE RESTRICT
+);
 show tables;
 #https://github.com/hadidy100/YummyTummy.git
